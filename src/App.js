05eb5e8ba506390466fbcceedcef533c62f79a9f
Yugandhar D S <yugandhar.subramanyam@softwareag.com>
2021-05-12 14:58:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import GreetingCat from "./components/GreetingCat";
+import RandomCat from "./components/RandomCat";
+import "./App.css";
+
+const defaultHistory = createBrowserHistory();
+
+function App({ history = defaultHistory }) {
+  return (
+    <Router history={history}>
+      <Route exact path="/" component={RandomCat} />
+      <Route exact path="/cat/:greeting" component={GreetingCat} />
+    </Router>
+  );
+}
+
+
+export default App;
